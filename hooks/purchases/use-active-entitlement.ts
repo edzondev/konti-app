74ae2c27b503c4964100457c5e6cf8c1f6af8d57
Edzon Perez <u17203330@utp.edu.pe>
2026-08-11 18:34:44 -0500

    chore: setup initial monorepo

--- hooks/purchases/use-active-entitlement.ts
+++
@@ -1,8 +0,0 @@
-import { usePurchases } from './use-purchases';
-import { hasActiveEntitlement } from '@/services/purchases';
-
-export function useActiveEntitlement(entitlementId?: string) {
-  const { customerInfo } = usePurchases();
-
-  return hasActiveEntitlement(customerInfo, entitlementId);
-}
